af50b237152562b893b8f3332872061ba2373ff1
BrighamAviles <avilesbrigham14@gmail.com>
2024-06-15 12:43:08 -0600

    list of numbers made and given

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+        {
+        List<int> numbers = new List<int>();
+        int input = -1; 
+
+        Console.WriteLine("Enter a series of numbers. Enter 0 to stop:");
+
+        while (input != 0)
+        {
+            Console.Write("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Sum of the numbers: " + sum);
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine("Average of the numbers: " + average);
+
+        int maxNumber = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine("Maximum number: " + maxNumber);
     }
 }
