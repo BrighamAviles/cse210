ab87916d90d8260397a920803f2c5b7b399fd5d7
BrighamAviles <avilesbrigham14@gmail.com>
2024-06-15 11:57:16 -0600

    crated a grading response to an imput of number

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercentage;
+
+        // Ensure the input is a valid integer
+        if (int.TryParse(input, out gradePercentage))
+        {
+            string letter = "";
+
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            // Print the letter grade
+            Console.WriteLine("Your letter grade is: " + letter);
+
+            // Determine if the user passed the course
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations, you passed the course!");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass the course. Better luck next time!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid grade percentage.");
+        }
     }
 }
